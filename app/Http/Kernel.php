e6750fb1516b432be57de841ca51481c4007db42
loy <jamesloupoli03@gmail.com>
2023-05-20 18:36:56 +0800

    added admin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         // custom middleware
-        'staff' => \App\Http\Middleware\MustBeStaff::class
+        'staff' => \App\Http\Middleware\MustBeStaff::class,
+        'admin' => \App\Http\Middleware\MustBeAdministrator::class
     ];
 }
