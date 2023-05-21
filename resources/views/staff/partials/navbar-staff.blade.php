52d27b35d4fd9ba3bf5bccd688bb73f7381ba17f
loy <jamesloupoli03@gmail.com>
2023-05-21 18:43:18 +0800

    added new navbar for admin-staff

---
+++ resources/views/staff/partials/navbar-staff.blade.php
@@ -0,0 +1,34 @@
+<li class="nav-item bottom-transition mx-1">
+    <a class="nav-link text-light" href="{{ route('home') }}">{{ __('HOME') }}</a>
+</li>
+<li class="nav-item bottom-transition mx-1">
+    <a class="nav-link text-light" href="{{ route('event') }}">{{ __('EVENTS') }}</a>
+</li>
+
+<li class="nav-item dropdown bottom-transition">
+    <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button"
+        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+        {{ strtoupper(Auth::guard('staff')->user()->first_name . ' ' . Auth::guard('staff')->user()->last_name) }}
+
+    </a>
+    <div class="dropdown-menu dropdown-menu-end bg-dark" aria-labelledby="navbarDropdown">
+        @staff
+            <a class="dropdown-item text-white" href="{{ route('profile.show') }}">
+                {{ __('PROFILE') }} </a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item text-white" href="{{ route('history.show') }}">
+                {{ __('HISTORY') }} </a>
+            <div class="dropdown-divider"></div>
+
+            <a class="dropdown-item text-white" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                {{ __('LOGOUT') }}
+            </a>
+        @endstaff
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
+    </div>
+
+</li>
