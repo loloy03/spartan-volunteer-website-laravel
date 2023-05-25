9b3fbb8304794b6e60cde00ca7d09615e5ca1020
loy <jamesloupoli03@gmail.com>
2023-05-25 14:58:24 +0800

    fixing conditionals in join as volunteer

--- resources/views/join-as-volunteer.blade.php
+++ resources/views/join-as-volunteer.blade.php
@@ -58,70 +58,64 @@
                             </div>
 
                         </div>
-                        <div class="col-lg-5 ">
-                            <div class="text-muted fs-10 f-montserrat ">PLEASE CHECK IN AND CHECK OUT IN THE DAY OF THE
-                                EVENT
-                            </div>
-                            <div class="mt-4">
-                                <div class="d-inline-block">
-                                    <form method="POST" action="{{ route('join_as_volunteer.check_in') }}">
-                                        @csrf
-                                        <input type="hidden" name="volunteer_id" value="{{ Auth::user()->volunteer_id }}">
-                                        <input type="hidden" name="event_id" value="{{ $event->event_id }}">
-                                        <!-- Cancel registration button -->
-                                        <button type="submit"
-                                            class="view-event-btn f-montserrat {{ $event->date != $today || $check_in != null ? 'view-event-btn-disabled' : '' }}"
-                                            {{ $event->date != $today || $check_in != null ? 'disabled' : '' }}>
-                                            Check In
-                                        </button>
-                                    </form>
-                                </div>
-                                <div class="d-inline-block mt-1">
-                                    <form method="POST" action="{{ route('join_as_volunteer.check_out') }}">
-                                        @csrf
-                                        <input type="hidden" name="volunteer_id" value="{{ Auth::user()->volunteer_id }}">
-                                        <input type="hidden" name="event_id" value="{{ $event->event_id }}">
-                                        <!-- Cancel registration button -->
-                                        <button type="submit"
-                                            class="view-event-btn f-montserrat {{ $event->date != $today || $proof_of_checkout == null || $check_out != null ? 'view-event-btn-disabled' : '' }}"
-                                            {{ $event->date != $today || $proof_of_checkout == null || $check_out != null ? 'disabled' : '' }}>
-                                            Check Out
-                                        </button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
                     </div>
-                    <div class=" {{ $check_in == null ? 'd-none' : '' }}">
-                        <div class="text-mute w-75 mx-auto">
-                            <hr>
+                    <div class="text-mute w-75 mx-auto">
+                        <hr>
+                    </div>
+                    <div class="my-3">
+                        <div class="f-montserrat fs-5 ">MY VOLUNTEER INFO</div>
+                        <div class="text-muted fs-10 f-montserrat ">PLEASE CHECK IN AND CHECK OUT IN THE DAY OF THE
+                            EVENT
                         </div>
-                        <div class="my-3">
-                            <div class="f-montserrat fs-5 ">MY VOLUNTEER INFO</div>
-                            <div class="row my-3">
-                                <div class="col-lg-6">
-                                    <div class="px-4">
-                                        <div class="f-montserrat mt-1">CHECKED IN TIME: <div class="f-lato">
-                                                {{ $check_in }}
-                                            </div>
-                                        </div>
-                                    </div>
+
+                        <div class="p-3">
+                            <div
+                                class="text-warning f-montserrat text-center border border-warning p-4 rounded mt-3
+                        @if ($check_out != null && $proof_of_checkout != null && $check_in != null && $attendance_status != 'validated') d-block
+                        @else
+                            d-none @endif">
+                                Please wait for the staff to validate your volunteer attendance
+                            </div>
+
+                            <div class="mt-3 {{ $attendance_status != 'validated' ? 'd-none' : '' }}">
+                                <div class="text-success f-montserrat text-center border border-success p-4 rounded">
+                                    Your volunteer attendance has been validated by the staff
                                 </div>
-                                <div class="col-lg-6">
-                                    <div class="px-4">
-                                        <div class="f-montserrat mt-1">CHECKED OUT TIME: <div class="f-lato">
-                                                {{ $check_out }}
-                                            </div>
+                            </div>
+                            <div class="mt-3 {{ $check_in == null ? 'd-none' : '' }}">
+                                <div class="f-montserrat mt-1">CHECKED IN TIME: <div class="f-lato">
+                                        <div class="p-4">
+                                            {{ date('F j, Y g:i A', strtotime($check_in)) }}
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="mt-5 {{ $check_in == null ? 'd-none' : '' }}">
-                                <div class="px-4">
-                                    <div class="f-montserrat ">PHOTO IN THE EVENT: </div>
-                                    <div>
-                                        Preview:
-                                    </div>
+                            <div>
+                                <form method="POST" action="{{ route('join_as_volunteer.check_in') }}">
+                                    @csrf
+                                    <input type="hidden" name="volunteer_id" value="{{ Auth::user()->volunteer_id }}">
+                                    <input type="hidden" name="event_id" value="{{ $event->event_id }}">
+                                    <!-- Check-in button -->
+                                    <button type="submit"
+                                        class="view-event-btn f-montserrat mt-2 {{ $check_in != null || $event->date != $today ? 'view-event-btn-disabled d-none' : '' }}"
+                                        {{ $check_in != null || $event->date != $today ? 'disabled' : '' }}>
+                                        Check In
+                                    </button>
+                                </form>
+
+                            </div>
+                        </div>
+                        <div
+                            class="mt-3 {{ $check_in == null || $attendance_status == 'checked' || $attendance_status == 'validated' ? 'd-none' : '' }}">
+                            <div class="text-warning f-montserrat text-center border border-warning p-4 rounded">
+                                Please go to your staff to check your Check In
+                            </div>
+                        </div>
+                        <div class="mt-3 @if ($attendance_status == 'validated' || $attendance_status == 'checked') d-block @else d-none @endif">
+                            <div class="px-4">
+                                <div class="f-montserrat ">PHOTO IN THE EVENT: </div>
+                                <div class="m-3">
+                                    Preview:
                                     <div class="text-center mt-2">
                                         <img id="preview" src="#" alt="Preview"
                                             style="display: none; max-width: 200px;">
@@ -136,7 +130,7 @@
                                         <input type="hidden" name="event_id" value="{{ $event->event_id }}">
                                         <div class="mt-3 {{ $proof_of_checkout != null ? 'd-none' : '' }}">
                                             <input type="file" name="photo" id="photo" class="form-control"
-                                                onchange="previewPhoto(event)">
+                                                onchange="previewPhoto(event)" required>
 
                                             <div class="text-center mt-2">
                                                 <input type="submit" value="Upload"
@@ -145,22 +139,29 @@
                                         </div>
                                     </form>
 
-                                    <script>
-                                        function previewPhoto(event) {
-                                            var input = event.target;
-                                            var preview = document.getElementById('preview');
-                                            preview.style.display = input.files && input.files[0] ? 'block' : 'none';
+                                </div>
+                            </div>
 
-                                            if (input.files && input.files[0]) {
-                                                var reader = new FileReader();
-                                                reader.onload = function(e) {
-                                                    preview.src = e.target.result;
-                                                }
-                                                reader.readAsDataURL(input.files[0]);
-                                            }
-                                        }
-                                    </script>
+                            <div class="px-4 mt-5">
+                                <div class="f-montserrat mt-1">CHECKED OUT TIME: <div class="f-lato">
+                                        <div class="m-3">
+                                            {{ date('F j, Y g:i A', strtotime($check_out)) }}
+                                        </div>
+                                    </div>
+                                </div>
 
+                                <div class="d-inline-block mt-1">
+                                    <form method="POST" action="{{ route('join_as_volunteer.check_out') }}">
+                                        @csrf
+                                        <input type="hidden" name="volunteer_id" value="{{ Auth::user()->volunteer_id }}">
+                                        <input type="hidden" name="event_id" value="{{ $event->event_id }}">
+                                        <!-- Cancel registration button -->
+                                        <button type="submit"
+                                            class="view-event-btn f-montserrat {{ $event->date != $today || $proof_of_checkout == null || $check_out != null ? 'view-event-btn-disabled d-none' : '' }}"
+                                            {{ $event->date != $today || $proof_of_checkout == null || $check_out != null ? 'disabled' : '' }}>
+                                            Check Out
+                                        </button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
@@ -169,6 +170,20 @@
             </div>
         </div>
     </div>
-    </div>
-    </div>
+
+    <script>
+        function previewPhoto(event) {
+            var input = event.target;
+            var preview = document.getElementById('preview');
+            preview.style.display = input.files && input.files[0] ? 'block' : 'none';
+
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    preview.src = e.target.result;
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+    </script>
 @endsection
